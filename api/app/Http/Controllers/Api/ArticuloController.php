ae9c45e569bd0359e9a70066a41fe6f8200ebcbc
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-11-29 09:41:36 -0300

    se crean las solicitudes en el controlador

--- api/app/Http/Controllers/Api/ArticuloController.php
+++ api/app/Http/Controllers/Api/ArticuloController.php
@@ -3,47 +3,50 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Articulo;
 use Illuminate\Http\Request;
 
 class ArticuloController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $articulo = Articulo::all();
+        return $articulo;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+        $articulo = new Articulo();
+        $articulo->descripcion = $request->descripcion;
+        $articulo->precio = $request->precio;
+        $articulo->stock = $request->stock;
+        $articulo->save();
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
-        //
+        $articulo = Articulo::find($id);
+        return $articulo;
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
-        //
+        $articulo = Articulo::findOrFail($id);
+        $articulo->descripcion = $request->descripcion;
+        $articulo->precio = $request->precio;
+        $articulo->stock = $request->stock;
+        $articulo->save();
+        return $articulo;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
-        //
+        $articulo = Articulo::destroy($id);
+        return $articulo;
     }
 }
