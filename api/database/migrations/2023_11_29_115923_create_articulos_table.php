e74920170f3fc4368b825aebecbf9d4c6ce0afe0
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-11-29 09:42:38 -0300

    se agregan las tablas a la migracion

--- api/database/migrations/2023_11_29_115923_create_articulos_table.php
+++ api/database/migrations/2023_11_29_115923_create_articulos_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('articulos', function (Blueprint $table) {
             $table->id();
+            $table->string('descripcion');
+            $table->double('precio');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('articulos');
