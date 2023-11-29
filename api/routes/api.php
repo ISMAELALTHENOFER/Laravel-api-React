50bb4a58687cd6be0635bce05dffc3aeede5c076
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-11-29 09:43:11 -0300

    se agregan las rutas con sus metodos

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\ArticuloController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ArticuloController::class)->group(function () {
+    Route::get('/articulos', 'index');
+    Route::post('/articulo', 'store');
+    Route::get('/articulo/{id}', 'show');
+    Route::put('/articulo/{id}', 'update');
+    Route::delete('/articulo/{id}', 'destroy');
 });
