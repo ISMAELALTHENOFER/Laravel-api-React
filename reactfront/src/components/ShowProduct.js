3ee6e764c505fad4840256fa61bdef8b5c812583
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2023-11-30 23:13:50 -0300

    se crea la vista para mostrar productos

---
+++ reactfront/src/components/ShowProduct.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+import { Link } from 'react-router-dom'
+
+
+const ShowProduct = () => {
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ShowProduct
